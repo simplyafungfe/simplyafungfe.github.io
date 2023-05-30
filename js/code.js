1c51efae1622028cce2e4c7cfd186aeabccd3410
simplyafungfe <afungfeharriet@gmail.com>
2023-05-30 03:55:27 -0700

    skye8 landing page

---
+++ js/code.js
@@ -0,0 +1,17 @@
+const myCollection = document.getElementsByTagName("h2");
+for (let i = 0; i < myCollection.length; i++) {
+  myCollection[i].style.color = "purple";
+}
+
+var subtopic = document.getElementsByClassName("sub-topic");
+var i;
+for(i=0; i<sub-topic.length; i++){
+  sub-topic[i].addEventListener("click", function() {
+        this.classlist,toggle("active");
+        var subtopic = this.nextElementSiblings;
+        if
+        (subtopic.style.display="block"){
+
+});
+    }
+}
